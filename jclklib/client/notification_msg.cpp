43e4962492b4684061525e1726a26b6d9d88bd9c
Lai Peter Jun Ann <peter.jun.ann.lai@intel.com>
2024-05-06 17:10:55 +0800

    Add status_wait API function
    Signed-off-by: Lai Peter Jun Ann <peter.jun.ann.lai@intel.com>

--- jclklib/client/notification_msg.cpp
+++ jclklib/client/notification_msg.cpp
@@ -15,7 +15,9 @@ using namespace JClkLibCommon;
 using namespace JClkLibClient;
 
 JClkLibCommon::client_ptp_event client_ptp_data;
-JClkLibCommon::ptp_event proxy_data;
+JClkLibCommon::ptp_event proxy_data = {};
+JClkLibCommon::jcl_state jclCurrentState = {};
+JClkLibCommon::jcl_state_event_count jclCurrentEventCount = {};
 
 /** @brief Create the ClientNotificationMessage object
  *
@@ -88,6 +90,20 @@ PROCESS_MESSAGE_TYPE(ClientNotificationMessage::processMessage)
 		client_ptp_data.gmIdentity[5], client_ptp_data.gmIdentity[6],client_ptp_data.gmIdentity[7]);
 	printf("asCapable = %d\n\n", client_ptp_data.asCapable);
 	printf("CLIENT offset_event_count = %d, gmIdentity_event_count = %d, asCapable_event_count = %d, servo_state_event_count = %d\n\n", offset_event_count, gmIdentity_event_count, asCapable_event_count, servo_state_event_count);
+
+	jclCurrentState.gm_present = client_ptp_data.gmPresent > 0 ? true:false;
+	jclCurrentState.as_Capable = client_ptp_data.asCapable > 0 ? true:false;
+	jclCurrentState.offset_in_range = client_ptp_data.master_offset_within_boundary;
+	jclCurrentState.servo_locked = client_ptp_data.servo_state >= SERVO_LOCKED ? true:false;
+	/* TODO : checked for jclCurrentState.gm_changed based on GM_identity previously stored */
+
+	jclCurrentEventCount.gmPresent_event_count = client_ptp_data.gmIdentity_event_count;
+	jclCurrentEventCount.offset_in_range_event_count = client_ptp_data.offset_event_count;
+	jclCurrentEventCount.asCapable_event_count = client_ptp_data.asCapable_event_count;
+	jclCurrentEventCount.servo_locked_event_count = client_ptp_data.servo_state_event_count;
+
+	state.set_eventState (jclCurrentState);
+	state.set_eventStateCount (jclCurrentEventCount);
 
 	return true;
 }
