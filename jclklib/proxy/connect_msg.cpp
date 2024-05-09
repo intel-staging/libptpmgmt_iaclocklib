70d404c739c868fb5de6e9e3c7c16321788f8a2f
Noor Azura Ahmad Tarmizi <noor.azura.ahmad.tarmizi@intel.com>
2024-05-09 02:47:43 -0400

    jclklib: client data management: Regroup the client data accordingly
    Split the data structures.
    
    Signed-off-by: Noor Azura Ahmad Tarmizi <noor.azura.ahmad.tarmizi@intel.com>

--- jclklib/proxy/connect_msg.cpp
+++ jclklib/proxy/connect_msg.cpp
@@ -77,8 +77,7 @@ PROCESS_MESSAGE_TYPE(ProxyConnectMessage::processMessage)
 
 	/* check whether there is ptp4l available */
 	if (!pe.ptp4l_id) {
-		//return false;
-		PrintDebug("[AZU_HACK] ptp4l is not available !!! , we continue anyway");
+		return false;
 	}
 
 	newSessionId = Client::CreateClientSession();
