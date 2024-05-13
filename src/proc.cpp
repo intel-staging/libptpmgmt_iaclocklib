bcf7832fc8f53d90e3f0528bd7db0d7b09119a00
Song Yoong Siang <yoong.siang.song@intel.com>
2024-05-13 13:32:31 +0800

    Fix c2cpp conversion for servo state enum
    Ensure proper convertion between ptpmgmt_servoState_e and
    ptpmgmt::servoState_e.
    
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

--- src/proc.cpp
+++ src/proc.cpp
@@ -1242,6 +1242,7 @@ C1(TIME_STATUS_NP)
     a.fractional_nanoseconds = d.fractional_nanoseconds;
     a.gmPresent = d.gmPresent;
     memcpy(a.gmIdentity.v, d.gmIdentity.v, ClockIdentity_t::size());
+    a.servo_state = (ptpmgmt_servoState_e)d.servo_state;
 }
 C1(GRANDMASTER_SETTINGS_NP)
 {
@@ -1730,6 +1731,7 @@ C2(TIME_STATUS_NP)
     a.fractional_nanoseconds = d.fractional_nanoseconds;
     a.gmPresent = d.gmPresent;
     memcpy(a.gmIdentity.v, d.gmIdentity.v, ClockIdentity_t::size());
+    a.servo_state = (servoState_e)d.servo_state;
 }
 C2(GRANDMASTER_SETTINGS_NP)
 {
