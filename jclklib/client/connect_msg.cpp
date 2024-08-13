e567ad5422dc24321e82e7a9e98e4a961b79bf09
Lai Peter Jun Ann <peter.jun.ann.lai@intel.com>
2024-08-13 10:38:29 +0800

    jclklib: Fix coverity issue
    Signed-off-by: Lai Peter Jun Ann <peter.jun.ann.lai@intel.com>

--- jclklib/client/connect_msg.cpp
+++ jclklib/client/connect_msg.cpp
@@ -91,6 +91,7 @@ PROCESS_MESSAGE_TYPE(ClientConnectMessage::processMessage)
     PrintDebug("Current state.sessionId: " +
         to_string(currentClientState->get_sessionId()));
     this->set_msgAck(ACK_NONE);
+    lock.unlock();
     cv.notify_one(lock);
     return true;
 }
