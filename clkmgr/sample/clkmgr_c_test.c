f0cfaae316175c83cc84c051e27126515e91b9f9
Lai Peter Jun Ann <peter.jun.ann.lai@intel.com>
2024-09-12 16:22:26 +0800

    clkmgr: Fix sample apps not able to terminate issue
    This patch is to ensure that the sample applications can terminate
    gracefully upon receiving a termination signal.
    
    Signed-off-by: Lai Peter Jun Ann <peter.jun.ann.lai@intel.com>

--- clkmgr/sample/clkmgr_c_test.c
+++ clkmgr/sample/clkmgr_c_test.c
@@ -17,8 +17,19 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include <clockmanager.h>
+
+
+typedef struct {
+    clkmgr_c_client_ptr client_ptr;
+    unsigned int timeout;
+    uint32_t idle_time;
+    struct clkmgr_c_event_state *event_state;
+    struct clkmgr_c_event_count *event_count;
+    struct clkmgr_c_subscription subscription;
+} thread_args_t;
 
 double getMonotonicTime() {
     struct timespec time_spec;
@@ -34,8 +45,93 @@ double getMonotonicTime() {
     return seconds + nanoseconds;
 }
 
+void *listen_clkmgr_c_status_wait(void *args) {
+    while(1) {
+        thread_args_t *thread_args = (thread_args_t *)args;
+        int retval = clkmgr_c_status_wait(thread_args->client_ptr, thread_args->timeout,
+            thread_args->event_state, thread_args->event_count);
+        if (!retval) {
+            printf("[clkmgr][%.3f] No event status changes identified in %d seconds.\n\n",
+                getMonotonicTime(), thread_args->timeout);
+            printf("[clkmgr][%.3f] sleep for %d seconds...\n\n",
+                getMonotonicTime(), thread_args->idle_time);
+            sleep(thread_args->idle_time);
+            continue;
+        } else if (retval < 0) {
+            printf("[clkmgr][%.3f] Terminating: lost connection to clkmgr Proxy\n",
+                getMonotonicTime());
+            return EXIT_SUCCESS;
+        }
+
+        printf("[clkmgr][%.3f] Waiting for Notification Event...\n",
+            getMonotonicTime());
+
+        printf("[clkmgr][%.3f] Obtained data from Notification Event:\n",
+            getMonotonicTime());
+        printf("+---------------------------+--------------+-------------+\n");
+        printf("| %-25s | %-12s | %-11s |\n", "Event", "Event Status",
+            "Event Count");
+        if (thread_args->subscription.event_mask) {
+        printf("+---------------------------+--------------+-------------+\n");
+        }
+        if (thread_args->subscription.event_mask & clkmgr_c_event_gm_offset) {
+            printf("| %-25s | %-12d | %-11d |\n", "offset_in_range",
+                thread_args->event_state->offset_in_range,
+                thread_args->event_count->offset_in_range_event_count);
+        }
+        if (thread_args->subscription.event_mask & clkmgr_c_event_synced2gm) {
+            printf("| %-25s | %-12d | %-11d |\n", "synced_to_primary_clock",
+               thread_args->event_state->synced_to_primary_clock, thread_args->event_count->synced_to_gm_event_count);
+        }
+        if (thread_args->subscription.event_mask & clkmgr_c_event_as_capable) {
+            printf("| %-25s | %-12d | %-11d |\n", "as_capable",
+                thread_args->event_state->as_capable, thread_args->event_count->as_capable_event_count);
+        }
+        if (thread_args->subscription.event_mask & clkmgr_c_event_gm_changed) {
+            printf("| %-25s | %-12d | %-11d |\n", "gm_Changed",
+                thread_args->event_state->gm_changed, thread_args->event_count->gm_changed_event_count);
+        }
+        printf("+---------------------------+--------------+-------------+\n");
+        printf("| %-25s |     %02x%02x%02x.%02x%02x.%02x%02x%02x     |\n",
+            "UUID", thread_args->event_state->gm_identity[0], thread_args->event_state->gm_identity[1],
+            thread_args->event_state->gm_identity[2], thread_args->event_state->gm_identity[3],
+            thread_args->event_state->gm_identity[4], thread_args->event_state->gm_identity[5],
+            thread_args->event_state->gm_identity[6], thread_args->event_state->gm_identity[7]);
+        printf("| %-25s |     %-19ld ns |\n",
+            "clock_offset", thread_args->event_state->clock_offset);
+        printf("| %-25s |     %-19ld ns |\n",
+            "notification_timestamp", thread_args->event_state->notification_timestamp);
+        printf("+---------------------------+--------------+-------------+\n");
+        if (thread_args->subscription.composite_event_mask) {
+            printf("| %-25s | %-12d | %-11d |\n", "composite_event",
+                   thread_args->event_state->composite_event, thread_args->event_count->composite_event_count);
+        }
+        if (thread_args->subscription.composite_event_mask & clkmgr_c_event_gm_offset) {
+            printf("| - %-23s | %-12s | %-11s |\n", "offset_in_range", "", "");
+        }
+        if (thread_args->subscription.composite_event_mask & clkmgr_c_event_synced2gm) {
+            printf("| - %-19s | %-12s | %-11s |\n", "synced_to_primary_clock", "", "");
+        }
+        if (thread_args->subscription.composite_event_mask & clkmgr_c_event_as_capable) {
+            printf("| - %-23s | %-12s | %-11s |\n", "as_capable", "", "");
+        }
+        if (thread_args->subscription.composite_event_mask) {
+            printf("+---------------------------+--------------+-------------+\n\n");
+        } else {
+            printf("\n");
+        }
+
+        printf("[clkmgr][%.3f] sleep for %d seconds...\n\n",
+            getMonotonicTime(), thread_args->idle_time);
+        sleep(thread_args->idle_time);
+
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    pthread_t thread;
+    thread_args_t thread_args;
     struct clkmgr_c_event_count event_count = {};
     struct clkmgr_c_subscription subscription = {};
     struct clkmgr_c_event_state event_state = {};
@@ -43,7 +139,6 @@ int main(int argc, char *argv[])
     int ret = EXIT_SUCCESS;
     uint32_t idle_time = 1;
     uint32_t timeout = 10;
-    int retval;
     int option;
 
     subscription.event_mask = (clkmgr_c_event_gm_offset | clkmgr_c_event_synced2gm |
@@ -206,81 +301,24 @@ int main(int argc, char *argv[])
 
     sleep(1);
 
-    while (1) {
-        printf("[clkmgr][%.3f] Waiting for Notification Event...\n",
-            getMonotonicTime());
-        retval = clkmgr_c_status_wait(client_ptr, timeout, &event_state , &event_count);
-        if (!retval) {
-            printf("[clkmgr][%.3f] No event status changes identified in %d seconds.\n\n",
-                getMonotonicTime(), timeout);
-            printf("[clkmgr][%.3f] sleep for %d seconds...\n\n",
-                getMonotonicTime(), idle_time);
-            sleep(idle_time);
-            continue;
-        } else if (retval < 0) {
-            printf("[clkmgr][%.3f] Terminating: lost connection to clkmgr Proxy\n",
-                getMonotonicTime());
-            return EXIT_SUCCESS;
-        }
+    // Initialize thread arguments
+    thread_args.client_ptr = client_ptr;
+    thread_args.timeout = timeout;
+    thread_args.idle_time = idle_time;
+    thread_args.event_state = &event_state;
+    thread_args.event_count = &event_count;
+    thread_args.subscription = subscription;
 
-        printf("[clkmgr][%.3f] Obtained data from Notification Event:\n",
-            getMonotonicTime());
-        printf("+---------------------------+--------------+-------------+\n");
-        printf("| %-25s | %-12s | %-11s |\n", "Event", "Event Status",
-            "Event Count");
-        if (subscription.event_mask) {
-        printf("+---------------------------+--------------+-------------+\n");
-        }
-        if (subscription.event_mask & clkmgr_c_event_gm_offset) {
-            printf("| %-25s | %-12d | %-11d |\n", "offset_in_range",
-                event_state.offset_in_range,
-                event_count.offset_in_range_event_count);
-        }
-        if (subscription.event_mask & clkmgr_c_event_synced2gm) {
-            printf("| %-25s | %-12d | %-11d |\n", "synced_to_primary_clock",
-               event_state.synced_to_primary_clock, event_count.synced_to_gm_event_count);
-        }
-        if (subscription.event_mask & clkmgr_c_event_as_capable) {
-            printf("| %-25s | %-12d | %-11d |\n", "as_capable",
-                event_state.as_capable, event_count.as_capable_event_count);
-        }
-        if (subscription.event_mask & clkmgr_c_event_gm_changed) {
-            printf("| %-25s | %-12d | %-11d |\n", "gm_Changed",
-                event_state.gm_changed, event_count.gm_changed_event_count);
-        }
-        printf("+---------------------------+--------------+-------------+\n");
-        printf("| %-25s |     %02x%02x%02x.%02x%02x.%02x%02x%02x     |\n",
-            "UUID", event_state.gm_identity[0], event_state.gm_identity[1],
-            event_state.gm_identity[2], event_state.gm_identity[3],
-            event_state.gm_identity[4], event_state.gm_identity[5],
-            event_state.gm_identity[6], event_state.gm_identity[7]);
-        printf("| %-25s |     %-19ld ns |\n",
-            "clock_offset", event_state.clock_offset);
-        printf("| %-25s |     %-19ld ns |\n",
-            "notification_timestamp", event_state.notification_timestamp);
-        printf("+---------------------------+--------------+-------------+\n");
-        if (subscription.composite_event_mask) {
-            printf("| %-25s | %-12d | %-11d |\n", "composite_event",
-                   event_state.composite_event, event_count.composite_event_count);
-        }
-        if (subscription.composite_event_mask & clkmgr_c_event_gm_offset) {
-            printf("| - %-23s | %-12s | %-11s |\n", "offset_in_range", "", "");
-        }
-        if (subscription.composite_event_mask & clkmgr_c_event_synced2gm) {
-            printf("| - %-19s | %-12s | %-11s |\n", "synced_to_primary_clock", "", "");
-        }
-        if (subscription.composite_event_mask & clkmgr_c_event_as_capable) {
-            printf("| - %-23s | %-12s | %-11s |\n", "as_capable", "", "");
-        }
-        if (subscription.composite_event_mask) {
-            printf("+---------------------------+--------------+-------------+\n\n");
-        } else {
-            printf("\n");
-        }
+    // Create the thread
+    if (pthread_create(&thread, NULL, listen_clkmgr_c_status_wait, &thread_args) != 0) {
+        perror("pthread_create");
+        return EXIT_FAILURE;
+    }
 
-        printf("[clkmgr][%.3f] sleep for %d seconds...\n\n",
-            getMonotonicTime(), idle_time);
-        sleep(idle_time);
+    // Wait for the thread to finish
+    if (pthread_join(thread, NULL) != 0) {
+        perror("pthread_join");
+        return EXIT_FAILURE;
     }
 
 do_exit:
