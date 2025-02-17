03b4e220935c1f04481dfa3578c6a8676f9a0f68
Lai Peter Jun Ann <peter.jun.ann.lai@intel.com>
2025-02-17 11:20:50 +0800

    clkmgr: Add support for dynamic UDS address for ptp4l and chrony.
    This patch enhances the clkmgr by adding support for dynamic Unix Domain
    Socket (UDS) addresses and domain numbers for ptp4l and chrony.
    
    Signed-off-by: Lai Peter Jun Ann <peter.jun.ann.lai@intel.com>

--- clkmgr/client/connect_msg.cpp
+++ clkmgr/client/connect_msg.cpp
@@ -52,9 +52,6 @@ PARSE_RXBUFFER_TYPE(ClientConnectMessage::parseBuffer)
     PrintDebug("[ClientConnectMessage]::parseBuffer ");
     if(!CommonConnectMessage::parseBuffer(LxContext))
         return false;
-    if(!PARSE_RX(FIELD, data.ptp4l_id, LxContext))
-        return false;
-    currentClientState->set_ptp4l_id(data.ptp4l_id);
     return true;
 }
 
