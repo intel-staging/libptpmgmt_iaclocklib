377e446265e7dbc74f7f52c7d92299864fbd777a
Song Yoong Siang <yoong.siang.song@intel.com>
2025-05-29 13:40:05 +0800

    clkmgr: utest: update ClockSyncSubscription test.
    Update subscription unit tests to use latest subscription class.
    
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

--- clkmgr/utest/subscription.cpp
+++ clkmgr/utest/subscription.cpp
@@ -2,76 +2,176 @@
    SPDX-FileCopyrightText: Copyright © 2024 Intel Corporation. */
 
 /** @file
- * @brief Subscription class unit tests
+ * @brief ClockSyncSubscription class unit tests
  *
  * @author Erez Geva <ErezGeva2@@gmail.com>
  * @copyright © 2024 Intel Corporation.
  *
  */
 
-#include "pub/clockmanager.h"
+#include "pub/clkmgr/subscription.h"
 
 using namespace clkmgr;
 
-// Tests event mask
-// void set_ClkMgrSubscription(const ClkMgrSubscription &newSubscription)
-TEST(SubscriptionTest, set_ClkMgrSubscription)
+// ClockSubscriptionBase() noexcept
+// uint32_t getEventMask() const
+// uint32_t getClockOffsetThreshold() const
+TEST(ClockSubscriptionBaseTest, DefaultConstructor_InitializesMembersToZero)
 {
-    ClkMgrSubscription subscription;
-    subscription.set_event_mask(eventGMOffset | eventSyncedToGM);
-    subscription.set_composite_event_mask(0x7945);
-    EXPECT_TRUE(subscription.define_threshold(thresholdGMOffset, 10, 5));
-    ClkMgrSubscription subscription2;
-    subscription2.set_ClkMgrSubscription(subscription);
-    EXPECT_EQ(subscription2.get_event_mask(), eventGMOffset | eventSyncedToGM);
-    EXPECT_EQ(subscription2.get_composite_event_mask(), 0x7945);
-    EXPECT_TRUE(subscription2.in_range(thresholdGMOffset, 8));
+    ClockSubscriptionBase sub;
+    EXPECT_EQ(sub.getEventMask(), 0u);
+    EXPECT_EQ(sub.getClockOffsetThreshold(), 0u);
 }
 
-// Tests event mask
-// void set_event_mask(const uint32_t &varname)
-// uint32_t get_event_mask() const
-TEST(SubscriptionTest, event_mask)
+// ClockSubscriptionBase() noexcept
+// void setEventMask(uint32_t)
+// uint32_t getEventMask() const
+TEST(ClockSubscriptionBaseTest, SetAndGetEventMask_StoresAndReturnsValue)
 {
-    ClkMgrSubscription subscription;
-    subscription.set_event_mask(eventGMOffset | eventSyncedToGM);
-    EXPECT_EQ(subscription.get_event_mask(), eventGMOffset | eventSyncedToGM);
-    EXPECT_TRUE(subscription.get_event_mask() & eventSyncedToGM);
-    EXPECT_FALSE(subscription.get_event_mask() & eventASCapable);
+    ClockSubscriptionBase sub;
+    sub.setEventMask(0xf);
+    EXPECT_EQ(sub.getEventMask(), 0xfu);
 }
 
-// Tests composite event mask
-// void set_composite_event_mask(uint32_t composite_event_mask)
-// uint32_t get_composite_event_mask() const
-TEST(SubscriptionTest, composite_event_mask)
+// ClockSubscriptionBase() noexcept
+// void setClockOffsetThreshold(uint32_t)
+// uint32_t getClockOffsetThreshold() const
+TEST(ClockSubscriptionBaseTest,
+    SetAndGetClockOffsetThreshold_StoresAndReturnsValue)
 {
-    ClkMgrSubscription subscription;
-    subscription.set_composite_event_mask(0x7945);
-    EXPECT_EQ(subscription.get_composite_event_mask(), 0x7945);
+    ClockSubscriptionBase sub;
+    sub.setClockOffsetThreshold(123456);
+    EXPECT_EQ(sub.getClockOffsetThreshold(), 123456u);
 }
 
-// Tests threshold
-// bool define_threshold(ThresholdIndex index, int32_t upper, int32_t lower)
-// bool get_threshold(ThresholdIndex index, int32_t &upper, int32_t &lower)
-// bool in_range(ThresholdIndex index, int32_t value) const
-TEST(SubscriptionTest, threshold)
+// ClockSubscriptionBase() noexcept
+// void setClockOffsetThreshold(uint32_t)
+// uint32_t getClockOffsetThreshold() const
+TEST(ClockSubscriptionBaseTest, SetClockOffsetThreshold_BoundaryValues)
 {
-    ClkMgrSubscription subscription;
-    // Index out of range
-    EXPECT_FALSE(subscription.define_threshold(thresholdLast, 10, 0));
-    // Wrong range
-    EXPECT_FALSE(subscription.define_threshold(thresholdGMOffset, 5, 10));
-    // Proper
-    EXPECT_TRUE(subscription.define_threshold(thresholdGMOffset, 10, 5));
-    // Index out of range
-    EXPECT_FALSE(subscription.in_range(thresholdLast, 0));
-    // value out of range
-    EXPECT_FALSE(subscription.in_range(thresholdGMOffset, 20));
-    // Proper
-    EXPECT_TRUE(subscription.in_range(thresholdGMOffset, 8));
-    int32_t upper = 0, lower = 0;
-    EXPECT_FALSE(subscription.get_threshold(thresholdLast, upper, lower));
-    EXPECT_TRUE(subscription.get_threshold(thresholdGMOffset, upper, lower));
-    EXPECT_EQ(upper, 10);
-    EXPECT_EQ(lower, 5);
+    ClockSubscriptionBase sub;
+    sub.setClockOffsetThreshold(0);
+    EXPECT_EQ(sub.getClockOffsetThreshold(), 0u);
+    sub.setClockOffsetThreshold(UINT32_MAX);
+    EXPECT_EQ(sub.getClockOffsetThreshold(), UINT32_MAX);
+}
+
+// PTPClockSubscription() noexcept
+// uint32_t getCompositeEventMask() const
+TEST(PTPClockSubscriptionTest,
+    DefaultConstructor_InitializesCompositeEventMaskToZero)
+{
+    PTPClockSubscription ptpSub;
+    EXPECT_EQ(ptpSub.getCompositeEventMask(), 0u);
+}
+
+// PTPClockSubscription() noexcept
+// void setCompositeEventMask(uint32_t)
+// uint32_t getCompositeEventMask() const
+TEST(PTPClockSubscriptionTest,
+    SetAndGetCompositeEventMask_StoresAndReturnsValue)
+{
+    PTPClockSubscription ptpSub;
+    ptpSub.setCompositeEventMask(0x7);
+    EXPECT_EQ(ptpSub.getCompositeEventMask(), 0x7);
+}
+
+// SysClockSubscription() noexcept
+// uint32_t getEventMask() const
+// uint32_t getClockOffsetThreshold() const
+TEST(SysClockSubscriptionTest, DefaultConstructor_InitializesBaseMembersToZero)
+{
+    SysClockSubscription sysSub;
+    EXPECT_EQ(sysSub.getEventMask(), 0u);
+    EXPECT_EQ(sysSub.getClockOffsetThreshold(), 0u);
+}
+
+// ClockSyncSubscription()
+// bool isPTPSubscriptionEnable() const
+// bool isSysSubscriptionEnable() const
+TEST(ClockSyncSubscriptionTest, DefaultConstructor_SubscriptionsDisabled)
+{
+    ClockSyncSubscription syncSub;
+    EXPECT_FALSE(syncSub.isPTPSubscriptionEnable());
+    EXPECT_FALSE(syncSub.isSysSubscriptionEnable());
+}
+
+// ClockSyncSubscription()
+// void enablePtpSubscription()
+// bool isPTPSubscriptionEnable() const
+// void disablePtpSubscription()
+TEST(ClockSyncSubscriptionTest, EnableDisablePTPSubscription)
+{
+    ClockSyncSubscription syncSub;
+    syncSub.enablePtpSubscription();
+    EXPECT_TRUE(syncSub.isPTPSubscriptionEnable());
+    syncSub.disablePtpSubscription();
+    EXPECT_FALSE(syncSub.isPTPSubscriptionEnable());
+}
+
+// ClockSyncSubscription()
+// void enableSysSubscription()
+// bool isSysSubscriptionEnable() const
+// void disableSysSubscription()
+TEST(ClockSyncSubscriptionTest, EnableDisableSysSubscription)
+{
+    ClockSyncSubscription syncSub;
+    syncSub.enableSysSubscription();
+    EXPECT_TRUE(syncSub.isSysSubscriptionEnable());
+    syncSub.disableSysSubscription();
+    EXPECT_FALSE(syncSub.isSysSubscriptionEnable());
+}
+
+// ClockSyncSubscription()
+// void setPtpSubscription(const PTPClockSubscription&)
+// const PTPClockSubscription& getPtpSubscription() const
+// PTPClockSubscription::setCompositeEventMask(uint32_t)
+// PTPClockSubscription::setEventMask(uint32_t)
+// PTPClockSubscription::setClockOffsetThreshold(uint32_t)
+// PTPClockSubscription::getCompositeEventMask() const
+// PTPClockSubscription::getEventMask() const
+// PTPClockSubscription::getClockOffsetThreshold() const
+TEST(ClockSyncSubscriptionTest, SetAndGetPtpSubscription)
+{
+    ClockSyncSubscription syncSub;
+    PTPClockSubscription ptpSub;
+    ptpSub.setCompositeEventMask(0x7);
+    ptpSub.setEventMask(0x8);
+    ptpSub.setClockOffsetThreshold(42);
+    syncSub.setPtpSubscription(ptpSub);
+    const PTPClockSubscription &result = syncSub.getPtpSubscription();
+    EXPECT_EQ(result.getCompositeEventMask(), 0x7u);
+    EXPECT_EQ(result.getEventMask(), 0x8u);
+    EXPECT_EQ(result.getClockOffsetThreshold(), 42u);
+}
+
+// ClockSyncSubscription()
+// void setSysSubscription(const SysClockSubscription&)
+// const SysClockSubscription& getSysSubscription() const
+// SysClockSubscription::setEventMask(uint32_t)
+// SysClockSubscription::setClockOffsetThreshold(uint32_t)
+// SysClockSubscription::getEventMask() const
+// SysClockSubscription::getClockOffsetThreshold() const
+TEST(ClockSyncSubscriptionTest, SetAndGetSysSubscription)
+{
+    ClockSyncSubscription syncSub;
+    SysClockSubscription sysSub;
+    sysSub.setEventMask(0x1);
+    sysSub.setClockOffsetThreshold(99);
+    syncSub.setSysSubscription(sysSub);
+    const SysClockSubscription &result = syncSub.getSysSubscription();
+    EXPECT_EQ(result.getEventMask(), 0x1u);
+    EXPECT_EQ(result.getClockOffsetThreshold(), 99u);
+}
+
+// PTPClockSubscription() noexcept
+// void setCompositeEventMask(uint32_t)
+// uint32_t getCompositeEventMask() const
+TEST(PTPClockSubscriptionTest, SetCompositeEventMask_BoundaryValues)
+{
+    PTPClockSubscription ptpSub;
+    ptpSub.setCompositeEventMask(0);
+    EXPECT_EQ(ptpSub.getCompositeEventMask(), 0u);
+    ptpSub.setCompositeEventMask(UINT32_MAX);
+    EXPECT_EQ(ptpSub.getCompositeEventMask(), UINT32_MAX);
 }
