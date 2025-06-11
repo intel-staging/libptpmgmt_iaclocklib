4852c9970b0d26505ff520d49a1303dce2f50b58
Song Yoong Siang <yoong.siang.song@intel.com>
2025-06-11 15:46:41 +0800

    build pass
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

--- clkmgr/utest/clockmanager.cpp
+++ clkmgr/utest/clockmanager.cpp
@@ -14,9 +14,8 @@
 #include "common/util.hpp"
 #include "common/termin.hpp"
 #include "client/timebase_state.hpp"
-#include <rtpi/condition_variable.hpp>
-#include <mutex>
-#include "client/timebase_state.hpp"
+//#include <rtpi/condition_variable.hpp>
+//#include <mutex>
 
 //#include "client/clock_event_handler.hpp"
 //#include "client/client_state.hpp"
@@ -49,18 +48,18 @@ bool ClientState::connect(uint32_t timeOut, timespec *lastConnectTime)
 bool TimeBaseStates::subscribe(size_t timeBaseIndex,
     const ClockSyncSubscription &newSub)
 {
-    if(newSub.isPTPSubscriptionEnable()) {
-        const PTPClockSubscription &newPtpSub = newSub.getPtpSubscription();
-        if(!setPtpEventSubscription(timeBaseIndex, newPtpSub))
-            return false;
-    }
+//    if(newSub.isPTPSubscriptionEnable()) {
+//        const PTPClockSubscription &newPtpSub = newSub.getPtpSubscription();
+//        if(!setPtpEventSubscription(timeBaseIndex, newPtpSub))
+ //           return false;
+  //  }
     // ToDo: Check whether system clock is available for subscription
-    if(newSub.isSysSubscriptionEnable()) {
-        const SysClockSubscription &newSysSub = newSub.getSysSubscription();
-        if(!setSysEventSubscription(timeBaseIndex, newSysSub))
-            return false;
-    }
-    setSubscribed(timeBaseIndex, true);
+//    if(newSub.isSysSubscriptionEnable()) {
+ //       const SysClockSubscription &newSysSub = newSub.getSysSubscription();
+//        if(!setSysEventSubscription(timeBaseIndex, newSysSub))
+//            return false;
+ //   }
+ //   setSubscribed(timeBaseIndex, true);
     return true;
 }
 
@@ -71,27 +70,53 @@ bool TimeBaseStates::getTimeBaseState(size_t timeBaseIndex,
     TimeBaseState &state)
 {
   //  lock_guard<rtpi::mutex> lock(mtx);
-    auto it = timeBaseStateMap.find(timeBaseIndex);
-    if(it != timeBaseStateMap.end()) {
-        state = it->second; // Copy the TimeBaseState object
+  //  auto it = timeBaseStateMap.find(timeBaseIndex);
+  //  if(it != timeBaseStateMap.end()) {
+    //    state = it->second; // Copy the TimeBaseState object
         // Get the current state of the timebase
-        PTPClockEvent ptp4lEventState = it->second.get_ptp4lEventState();
-        SysClockEvent chronyEventState = it->second.get_chronyEventState();
+ //       PTPClockEvent ptp4lEventState = it->second.get_ptp4lEventState();
+  //      SysClockEvent chronyEventState = it->second.get_chronyEventState();
         // Reset the Event Count
-        ptpClockEventHandler.setOffsetInRangeEventCount(ptp4lEventState, 0);
-        ptpClockEventHandler.setSyncedWithGmEventCount(ptp4lEventState, 0);
-        ptpClockEventHandler.setGmChangedEventCount(ptp4lEventState, 0);
-        ptpClockEventHandler.setAsCapableEventCount(ptp4lEventState, 0);
-        ptpClockEventHandler.setCompositeEventCount(ptp4lEventState, 0);
-        ptpClockEventHandler.setGmChanged(ptp4lEventState, false);
-        it->second.set_ptpEventState(ptp4lEventState);
-        sysClockEventHandler.setOffsetInRangeEventCount(chronyEventState, 0);
-        it->second.set_chronyEventState(chronyEventState);
-        it->second.set_event_changed(false);
-        return true;
-    }
+  //      ptpClockEventHandler.setOffsetInRangeEventCount(ptp4lEventState, 0);
+   //     ptpClockEventHandler.setSyncedWithGmEventCount(ptp4lEventState, 0);
+    //    ptpClockEventHandler.setGmChangedEventCount(ptp4lEventState, 0);
+     //   ptpClockEventHandler.setAsCapableEventCount(ptp4lEventState, 0);
+ //       ptpClockEventHandler.setCompositeEventCount(ptp4lEventState, 0);
+  //      ptpClockEventHandler.setGmChanged(ptp4lEventState, false);
+   //     it->second.set_ptpEventState(ptp4lEventState);
+    //    sysClockEventHandler.setOffsetInRangeEventCount(chronyEventState, 0);
+     //   it->second.set_chronyEventState(chronyEventState);
+//        it->second.set_event_changed(false);
+ //       return true;
+  //  }
     // If timeBaseIndex is not found, return false
-    return false;
+    return true;
+}
+
+const timespec &TimeBaseState::get_last_notification_time() const
+{
+    return last_notification_time;
+}
+
+
+bool TimeBaseState::get_subscribed() const
+{
+    return subscribed;
+}
+
+bool TimeBaseState::is_event_changed() const
+{
+    return event_changed;
+}
+
+const PTPClockEvent &TimeBaseState::get_ptp4lEventState() const
+{
+    return ptp4lEventState;
+}
+
+const SysClockEvent &TimeBaseState::get_chronyEventState() const
+{
+    return chronyEventState;
 }
 
 // static ClockManager &fetchSingleInstance()
